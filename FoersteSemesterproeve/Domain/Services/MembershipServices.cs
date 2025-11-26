c65372b31c93841630a9bf0c1f883f256798ee5e
Martin Nordbø <nordboe@live.dk>
2025-11-26 12:08:31 +0100

    Service layer started

---
+++ FoersteSemesterproeve/Domain/Services/MembershipServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoersteSemesterproeve.Domain.Services
+{
+    internal class MembershipServices
+    {
+
+    }
+}
+    
