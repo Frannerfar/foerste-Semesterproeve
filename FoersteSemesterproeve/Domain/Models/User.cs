f154d77903a26e5a837eda206d20393ba5835724
Martin Nordbø <nordboe@live.dk>
2025-11-26 20:34:10 +0100

    Updated User model's attributes and set string marks for roles, for better DataGrid overview.

--- FoersteSemesterproeve/Domain/Models/User.cs
+++ FoersteSemesterproeve/Domain/Models/User.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FoersteSemesterproeve.Domain.Models
 {
     public class User
     {
-        public int id;
+        public int id { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string email { get; set; }
@@ -17,6 +20,8 @@ namespace FoersteSemesterproeve.Domain.Models
         public string password;
         public bool isCoach;
         public bool isAdmin;
+        public string isCoachText { get; set; }
+        public string isAdminText { get; set; }
         public DateOnly dateofBirth;
         public int postal;
         public bool hasPaid;
@@ -37,7 +42,22 @@ namespace FoersteSemesterproeve.Domain.Models
             this.dateofBirth = dateofBirth;
             this.postal = postal;
             this.hasPaid = hasPaid;
+            this.isCoachText = SetMark(isCoach);
+            this.isAdminText = SetMark(isAdmin);
+        }
 
+        private string SetMark(bool statement)
+        {
+            if(statement)
+            {
+                //return "Yes";
+                return "✔";
+            }
+            else
+            {
+                //return "No";
+                return "❌";
+            }
         }
     }
 }
