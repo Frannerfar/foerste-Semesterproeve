d7e6f3eb6b2871a16058e1a771ff2f61975cf4c5
Martin Nordbø <nordboe@live.dk>
2025-11-29 17:02:57 +0100

    Added simple "active" state to menu

--- FoersteSemesterproeve/MainWindow.xaml.cs
+++ FoersteSemesterproeve/MainWindow.xaml.cs
@@ -27,6 +27,14 @@ namespace FoersteSemesterproeve
         ActivityService activityService;
         LocationService locationService;
 
+        SolidColorBrush menuStaticItem;
+        SolidColorBrush menuActiveItem;
+
+        Color menuStaticItemColor = Color.FromRgb(119, 136, 153);
+        Color menuActiveItemColor = Color.FromRgb(177, 211, 245);
+
+        Button currentActiveMenuButton;
+
         // Sæt til false, hvis der skal testes med login
         bool isDeveloping = true;
 
@@ -45,6 +53,12 @@ namespace FoersteSemesterproeve
             locationService = new LocationService();
             router = new NavigationRouter(MainContent, MenuGrid, UserProfileButton, adminButtons, userService, activityService, locationService, membershipService);
 
+            menuStaticItem = new SolidColorBrush(menuStaticItemColor);
+            menuActiveItem = new SolidColorBrush(menuActiveItemColor);
+
+            currentActiveMenuButton = HomeButton;
+            SetMenuButtonActive(currentActiveMenuButton, HomeButton);
+
             // Hvis vi "udvikler/tester"
             if (isDeveloping)
             {
@@ -59,33 +73,56 @@ namespace FoersteSemesterproeve
             }
         }
 
+        private void SetMenuButtonActive(Button originalButton, Button newButton)
+        {
+            if (originalButton != null) 
+            {
+                originalButton.Background = menuStaticItem;
+            }
+
+            newButton.Background = menuActiveItem;
+            currentActiveMenuButton = newButton;
+        }
+
         private void Home_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Home);
         }
 
         private void Activities_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Activities);
         }
 
         private void Trainers_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Trainers);
         }
 
         private void Locations_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Locations);
         }
 
         private void Members_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Members);
         }
 
         private void Memberships_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            SetMenuButtonActive(currentActiveMenuButton, button);
             router.Navigate(Route.Memberships);
         }
 
