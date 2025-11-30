15eb12e427f1be489946c385ab5966770a2b0f89
MarcusThomsen10 <marcusengelthomsen@gmail.com>
2025-11-30 23:16:39 +0100

    MembershipTypes all done

--- FoersteSemesterproeve/Presentation/Pages/EditMembershipTypePage.xaml.cs
+++ FoersteSemesterproeve/Presentation/Pages/EditMembershipTypePage.xaml.cs
@@ -32,8 +32,66 @@ namespace FoersteSemesterproeve.Presentation.Pages
             this.membershipService = membershipService;
             if(membershipService.targetMembershipType != null)
             {
-                MembershipTypeName.Text = membershipService.targetMembershipType.name;
+                TitleInput.Text = membershipService.targetMembershipType.name;
+                MonthlyInput.Text = membershipService.targetMembershipType.monthlyPayDKK.ToString();
+                YearlyInput.Text = membershipService.targetMembershipType.yearlyPayDKK.ToString();
             }
+        }
+
+        private void CancelAddMembershipTypeButton_Click(object sender, RoutedEventArgs e)
+        {
+            router.Navigate(NavigationRouter.Route.Memberships);
+        }
+
+        private void SaveNewMembershipTypeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(membershipService.targetMembershipType != null)
+            {
+                if(TitleInput.Text == null || MonthlyInput.Text == null || YearlyInput.Text == null)
+                {
+                    MessageBox.Show("You have to fill out all boxes");
+                    return;
+                }
+
+                if(TitleInput.Text != null && MonthlyInput.Text != null && YearlyInput != null)
+                {
+                    bool isMonthlyInteger = int.TryParse(MonthlyInput.Text, out int monthlyInputInteger);
+                    bool isYearlyInteger = int.TryParse(YearlyInput.Text, out int yearlyInputInteger);
+
+                    if(isMonthlyInteger == true)
+                    {
+                        membershipService.targetMembershipType.monthlyPayDKK = monthlyInputInteger;
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("You have to write a number in Monthly Pay");
+                        return;
+                    }
+
+                    if (isYearlyInteger == true)
+                    {
+                        membershipService.targetMembershipType.yearlyPayDKK = yearlyInputInteger;
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("You have to write a number in Yearly Pay");
+                        return;
+                    }
+
+                    membershipService.targetMembershipType.name = TitleInput.Text;
+
+                    router.Navigate(NavigationRouter.Route.Memberships);
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("TargetMembershipType is null");
+            }
+
         }
     }
 }
