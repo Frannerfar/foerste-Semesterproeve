2527b48943c44db388cba0639144b7d0c8812f59
Martin Nordbø <nordboe@live.dk>
2025-12-10 20:11:13 +0100

    Populating activities with users randomly

--- FoersteSemesterproeve/Domain/Services/ActivityService.cs
+++ FoersteSemesterproeve/Domain/Services/ActivityService.cs
@@ -23,14 +23,21 @@ namespace FoersteSemesterproeve.Domain.Services
             this.userService = userService;
             this.activities = new List<Activity>();
             GenerateDummyActivities();
+
+            PopulateActivityWithDummyUsers(activities[0]);
+            for (int i = 1; i < activities.Count; i++) 
+            {
+                PopulateActivityRandomlyWithUsers(activities[i]);
+            }
         }
 
         
         //Hvordan en activity bliver oprettet når en admin oprettet det:
-        public void AddActivity(string title, User? coach, Location location, int? maxCapacity, DateTime startTime, DateTime endTime) 
+        public Activity AddActivity(string title, User? coach, Location location, int? maxCapacity, DateTime startTime, DateTime endTime) 
         {
             Activity newActivity = new Activity(title, coach, location, maxCapacity, startTime, endTime);
             activities.Add(newActivity);
+            return newActivity;
         }
 
         public void DeleteActivity(Activity activity)
@@ -77,13 +84,114 @@ namespace FoersteSemesterproeve.Domain.Services
 
         private void GenerateDummyActivities()
         {
-            //AddActivity("Test", userService.users[0], locationService.locations[0], 2, DateTime.Now.AddHours(1).AddMinutes(45), DateTime.Now.AddHours(2).AddMinutes(15));
-            AddActivity("Mordor Marathon", null, locationService.locations[0], 7, DateTime.Now.AddHours(9).AddMinutes(20), DateTime.Now.AddHours(2).AddMinutes(45));
-            AddActivity("Second Breakfast Yoga", null, locationService.locations[0], 9, DateTime.Now.AddHours(3).AddMinutes(40), DateTime.Now.AddHours(2).AddMinutes(0));
-            AddActivity("Spinning med Viggo", null, locationService.locations[0], 3, DateTime.Now.AddHours(6).AddMinutes(0), DateTime.Now.AddHours(2).AddMinutes(15));
-            AddActivity("Refleks Træning med Hugo", null, locationService.locations[0], 5, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
-            AddActivity("Winter is Coming Powerlift", null, locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                //AddActivity("Test", userService.users[0], locationService.locations[0], 2, DateTime.Now.AddHours(1).AddMinutes(45), DateTime.Now.AddHours(2).AddMinutes(15));
+                AddActivity("Spinning med Viggo", null, locationService.locations[0], 6, DateTime.Now.AddHours(6).AddMinutes(0), DateTime.Now.AddHours(2).AddMinutes(15));
+                AddActivity("Mordor Maratonløb", null, locationService.locations[0], 7, DateTime.Now.AddHours(9).AddMinutes(20), DateTime.Now.AddHours(2).AddMinutes(45));
+                AddActivity("Second Breakfast Yoga", null, locationService.locations[0], 9, DateTime.Now.AddHours(3).AddMinutes(40), DateTime.Now.AddHours(2).AddMinutes(0));
+                AddActivity("Kampsport med Hugo", null, locationService.locations[0], 5, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                AddActivity("'Winter is Coming' Powerlift", null, locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                AddActivity("Isengaard CrossFit", null, locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                AddActivity("Øksekast med John", null, locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                AddActivity("Fellowship gruppetræning", null, locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
+                // Bueskydning hvor vi tilføjer Orlando Bloom som træner (userService.users[8]. Derudover dynamisk tid.
+                AddActivity("Bueskydning for meget øvede", userService.users[8], locationService.locations[0], null, DateTime.Now.AddHours(20).AddMinutes(3), DateTime.Now.AddHours(1).AddMinutes(30));
         }
 
+
+        private void PopulateActivityWithDummyUsers(Activity activity)
+        {
+            // Hvis aktiviteten ikke er null (og dermed er begrænset i antal af deltagere)
+            if (activity.maxCapacity != null)
+            {
+                // Vi looper hver 
+                for (int i = 0; i < activity.maxCapacity; i++)
+                {
+                    // Her tilføjes user til aktiviteten
+                    activity.participants.Add(userService.users[i+1]);
+                    // Her tilføjes aktiviteten til user
+                    userService.users[i+1].activityList.Add(activity);
+                }
+            }
+            else
+            {
+                // Populate med de 10 første users
+                for (int i = 0; i < 10; i++)
+                {
+                    // Her tilføjes user til aktiviteten
+                    activity.participants.Add(userService.users[i]);
+                    // Her tilføjes aktiviteten til user
+                    userService.users[i].activityList.Add(activity);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Bruges til at tilføje tilfældige mængder af tilfældige brugere til en aktivitet
+        /// </summary>
+        /// <author>Martin</author>
+        /// <param name="activity"></param>
+        private void PopulateActivityRandomlyWithUsers(Activity activity)
+        {
+            // Instantiering af Random, så vi kan generere tilfældige tal
+            Random random = new Random();
+            // Variabel vi bruger til at indeholde antallet af users vi tilmelder
+            int randomAmountOfPeopleToJoin = 0;
+
+            // HashSet er en unik liste. Vi bruger den til at undgå duplicates af Users, i stedet for en List
+            HashSet<User> hashedUsers = new HashSet<User>();
+
+            // Hvis ikke aktiviteten er null (hvis null, så håndterer vi det som ubegrænset mængde)
+            if (activity.maxCapacity != null)
+            {
+                // Vi sætter variablen til at være mellem 1 og aktivitetens maksimum deltagere
+                randomAmountOfPeopleToJoin = random.Next(1, (int)activity.maxCapacity);
+
+                // Så længe at listen af unikke brugere er mindre end variablen er sat til, af personer vi gerne vil have tilføjet
+                while (hashedUsers.Count < randomAmountOfPeopleToJoin)
+                {
+                    // Tilfældig user tilføjes til den unikke liste
+                    // Her bruges userService.users (som indeholder alle users i programmet) og random objektet til at generere
+                    // et tal mellem 0 og antallet af users i programmet
+                    hashedUsers.Add(userService.users[random.Next(0, userService.users.Count)]);
+                    // Hvis User allerede eksisterer i den unikke liste, så bliver den naturligvis ikke tilføjet - og dermed
+                    // fortsætter loopet indtil det har det rigtige antal af unikke User i sig.
+                }
+
+                // For hver bruger/User i den unikke liste hashedUsers, til føjer vi aktiviteten til brugeren og brugeren til aktiviteten
+                foreach (User user in hashedUsers)
+                {
+                    // Her tilføjes user til aktiviteten
+                    activity.participants.Add(user);
+                    // Her tilføjes aktiviteten til user
+                    user.activityList.Add(activity);
+                }
+            }
+            // Ubegrænset antal deltagere, hvis aktivitetens maxCapacity ER null
+            else
+            {
+                // Vi sætter variablen til at være mellem 1 og 10, for at have et fornuftigt antal at populate aktiviteten med
+                randomAmountOfPeopleToJoin = random.Next(1, 10);
+
+                // Så længe at listen af unikke brugere er mindre end antallet af personer vi gerne vil have tilføjet
+                while(hashedUsers.Count < randomAmountOfPeopleToJoin)
+                {
+                    // Tilfældig user tilføjes til den unikke liste
+                    // Her bruges userService.users (som indeholder alle users i programmet) og random objektet til at generere
+                    // et tal mellem 0 og antallet af users i programmet
+                    hashedUsers.Add(userService.users[random.Next(0, userService.users.Count)]);
+                    // Hvis User allerede eksisterer i den unikke liste, så bliver den naturligvis ikke tilføjet - og dermed
+                    // fortsætter loopet indtil det har det rigtige antal af unikke User i sig.
+                }
+
+                // For hver bruger/User i den unikke liste hashedUsers, til føjer vi aktiviteten til brugeren og brugeren til aktiviteten
+                foreach(User user in hashedUsers)
+                {
+                    // Her tilføjes user til aktiviteten
+                    activity.participants.Add(user);
+                    // Her tilføjes aktiviteten til user
+                    user.activityList.Add(activity);
+                }
+            }
+        }
     }
 }
